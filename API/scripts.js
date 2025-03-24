8a12c175a4d94f0c3b2cfd08cf98015879bcc3de
Colby <maquiret@gmail.com>
2025-03-23 21:31:23 -0500

    phil session

--- API/scripts.js
+++ API/scripts.js
@@ -1,1 +1,13 @@
 //https://www.omdbapi.com/ Open API use for later
+const OMDB_API_KEY = "48ab736e";
+
+// https://www.omdbapi.com/?apikey=[yourkey]&
+
+const omdbSearchReq = async (search) => {
+  const res = await fetch(`https://www.omdbapi.com?apikey=${OMDB_API_KEY}&s=${search}`)
+  const toJson = await res.json()
+  return toJson
+} 
+
+// https://www.omdbapi.com/?apikey=48ab736e&s=spiderman
+
