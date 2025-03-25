a2b27813feede1e752759cacf2ed29ad0116f02c
Colby <maquiret@gmail.com>
2025-03-25 01:40:27 -0500

    added some js

--- API/scripts.js
+++ API/scripts.js
@@ -5,11 +5,11 @@ const OMDB_API_KEY = "48ab736e";
 
 const omdbSearchReq = async (search) => {
   const res = await fetch(`https://www.omdbapi.com?apikey=${OMDB_API_KEY}&s=${search}`)
-  const poster = await fetch ('http://img.omdbapi.com/?apikey=[yourkey]&')
+  const poster = await fetch (`http://img.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${search}`)
   const toJson = await res.json()
   return toJson
 } 
 
-//http://img.omdbapi.com/?apikey=[yourkey]&
+//http://img.omdbapi.com/?apikey=${OMDB_API_KEY}&
 // https://www.omdbapi.com/?apikey=48ab736e&s=spiderman
 
--- Saved-items/scripts.js
+++ Saved-items/scripts.js
@@ -1,3 +1,4 @@
+const OMDB_API_KEY = "48ab736e";
 const inputBox = document.getElementById('searchInput');
 const listContainer = document.getElementById("list-container");
 
@@ -8,23 +9,28 @@ window.onload = function () {
         }
 }
 
-
+//function for creating divs for saved items
+function createList() {
+    let div = document.createElement('div');
+    div.innerHTML = inputBox.value;
+    listContainer.appendChild(div);
+}
 function addList(){
  if(inputBox.value === '') {
     alert("Please search the catalogue")
    }
-  else{
+  /*else{
       let li = document.createElement('li');
       li.innerHTML = inputBox.value;
       listContainer.appendChild(li);
       let span = document.createElement("span")
       span.innerHTML = "X"
       li.appendChild(span);
-    }
+    }*/
     inputBox.value = "";
 };
 
-listContainer.addEventsListener("click", function(e) {
+listContainer.addEventListener("click", function(e) {
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
     } else if(e.target.tagName === "SPAN"){
@@ -32,9 +38,72 @@ listContainer.addEventsListener("click", function(e) {
     }
 }, false);
 
-//css boxes for unordered lists
-function createList() {
-    let li = document.createElement('li');
-    li.innerHTML = inputBox.value;
-    listContainer.appendChild(li);
-}
+//MODAL FOR DIV ITEMS
+/*async function fetchData() {
+    try {
+        const food = foodInput.value.trim();
+
+        const apiKey = "PKukaoIj0EkAALXfan9OXgKGpXNwcXKazqVKSoci";
+        let apiUrl = https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${apiKey}&query=${food};
+
+        const response = await axios.get(apiUrl);
+
+        if (
+          response.data &&
+          response.data.foods &&
+          response.data.foods.length > 0
+        ) {
+          displayResults(response.data.foods);
+        } else {
+          resultsDiv.innerHTML = "<p>No results found.</p>";
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        resultsDiv.innerHTML = "<p>An error occurred. Please try again.</p>";
+      }
+    }
+
+    function displayResults(foods) {
+      resultsDiv.innerHTML = "";
+
+      foods.forEach((foodItem) => {
+        const desc = foodItem.description || "Description not available";
+
+        const div = document.createElement("div");
+        div.classList.add("result-item");
+        div.innerHTML = <p><strong></strong> ${desc}</p>;
+
+        resultsDiv.appendChild(div);
+
+        div.addEventListener("click", function () {
+          divInfo(foodItem); 
+        });
+      });
+    }*/
+
+//data request for movies
+
+//figure out how to pull posters from API
+const omdbSearchReq = async (search) => {
+    const res = await fetch(`https://www.omdbapi.com?apikey=${OMDB_API_KEY}&s=${search}`)
+    const poster = await fetch (`http://img.omdbapi.com/?apikey=${OMDB_API_KEY}&${search}`)
+    const toJson = await res.json()
+    return toJson
+  } 
+
+searchButton.onclick = async function() {
+    console.log("HELLO WORLD")
+    const searchVal = searchInput.value
+    const results = await omdbSearchReq(searchVal)
+    console.log("results =", results)
+    for(let i = 0; i < results.Search.length; i++) {
+      const pre = document.createElement("pre")
+      pre.textContent = JSON.stringify(results.Search[i], null, 2)
+      const div = document.createElement("div")
+      div.classList.add('li')
+      div.appendChild(pre)
+      listContainer.appendChild(div)
+    }
+    
+
+  }
